b6e06e2902b2da84f0af777d4d8033b08d6e7877
Josua44 <103715118+Josuwata44@users.noreply.github.com>
2022-07-18 18:09:28 +0900

    Initial Commit

---
+++ GeometryReaderQuiz/GeometryReaderQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderQuizApp.swift
+//  GeometryReaderQuiz
+//
+//  Created by cmStudent on 2022/07/18.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

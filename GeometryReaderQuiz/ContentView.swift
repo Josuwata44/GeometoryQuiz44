4bd9b8c78bfe5337c3c537e11b2acaba86d93f15
watajosu <21cm0144@jec.ac.jp>
2022-10-15 19:15:14 +0900

    first commit

--- GeometryReaderQuiz/ContentView.swift
+++ GeometryReaderQuiz/ContentView.swift
@@ -86,21 +86,17 @@ struct ContentView: View {
                     
                     VStack {
                         Image("Image")
-                            .resizable()
-                        BounceAnimationView(text: "クリア", startTime: 0.0)
+                            .foregroundColor(.white)
                             .font(.largeTitle)
-                            .foregroundColor(.red)
-                            .frame(width: geometry.size.width / 2, height: 10)
+                            .frame(width: geometry.size.width / 2, height: 50)
+                        
                            
-                        BounceAnimationView(text: "あなたの得点は", startTime: 0.0)
+                        Text("得点:\(userScore)")
                             .font(.title)
                             .foregroundColor(.white)
                             .frame(width: geometry.size.width / 2, height: 10)
                            
-                        BounceAnimationView(text: "\(userScore)", startTime: 0.0)
-                            .font(.largeTitle)
-                            .foregroundColor(userScore > 0 ? .green : .red)
-                            .frame(width: geometry.size.width / 2, height: 10)
+                        
                          
                         Button("Restart") {
                             self.Question()
@@ -181,46 +177,7 @@ struct OneImage: View {
     
 }
 
-struct BounceAnimationView: View {
-    let characters: Array<String.Element>
 
-    @State var offsetYForBounce: CGFloat = -50
-    @State var opacity: CGFloat = 0
-    @State var baseTime: Double
-
-    init(text: String, startTime: Double){
-        self.characters = Array(text)
-        self.baseTime = startTime
-    }
-
-    var body: some View {
-        HStack(spacing:0){
-            ForEach(0..<characters.count) { num in
-                Text(String(self.characters[num]))
-                    .font(.custom("HiraMinProN-W3", fixedSize: 48))
-                    .offset(x: 0, y: offsetYForBounce)
-                    .opacity(opacity)
-                    .animation(.spring(response: 0.2, dampingFraction: 0.5, blendDuration: 0.1).delay( Double(num) * 0.1 ), value: offsetYForBounce)
-            }
-            .onTapGesture {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.0) {
-                    opacity = 0
-                    offsetYForBounce = -50
-                }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
-                    opacity = 1
-                    offsetYForBounce = 0
-                }
-            }
-            .onAppear{
-                DispatchQueue.main.asyncAfter(deadline: .now() + (0.8 + baseTime)) {
-                    opacity = 1
-                    offsetYForBounce = 0
-                }
-            }
-        }
-    }
-}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
